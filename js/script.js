5fe189c500b4b8572bd95846c093db10fbfd2ef4
Dennis Pavitschitz <d.pavitschitz@hotmail.com>
2019-10-12 09:12:37 +0200

    Add design with json input

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 var data = JSON.parse(movie);
+
+$("document").ready(function(){
+	for (let i = 0; i < data.length; i++) {
+		$("#movieRow").append(`<div class="col-sm-5 py-2"><div class="row movie-container"><div class="col-sm-5"><img src=${data[i].image}>           </div><div class="col-sm-6"><p class="text-light">${data[i].name}</p><p class="text-light">${data[i].description}</p><p class="text-light text-right text-success">like <img class="thump" src=${data[i].likeThump}><button type="button" class="btn  btn-success circle-button">0</button></p></div></div></div>`);
+	}
+    	function addToCart(){
+		var i = this.name;
+			data[i].like++;
+			update();
+	}
+    
+});
+
+
+              
